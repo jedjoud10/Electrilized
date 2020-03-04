b8b9678e27d041aeac3a7e0fd35ca6e7eba28c33
Jedjoud10 <jribijed6@gmail.com>
2020-03-04 18:46:59 -0500

    First commit wooo

--- Assets/Other/Scripts/GameConfigHandlerScript.cs
+++ Assets/Other/Scripts/GameConfigHandlerScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 //A handler script that handles information flow between GameConfigSaverLoader class and the game config itself
 public class GameConfigHandlerScript : MonoBehaviour
 {
@@ -20,12 +21,24 @@ public class GameConfigHandlerScript : MonoBehaviour
     private GameConfig SaveConfig()
     {
         GameConfig outconfig = new GameConfig();
+        //Post-processing
+        outconfig.usePostProcessing = true;
+        outconfig.useColorGrading = true;
+        outconfig.useChromaticAberration = true;
+        outconfig.useBloom = true;
+        outconfig.useVignette = true;
+        outconfig.useAutoExposure = true;
+        outconfig.useMotionBlur = true;
+        outconfig.useAmbientOcclusion = true;
+        outconfig.useAntiAliasing = 1;
+
         outconfig.PixelLightCount = QualitySettings.pixelLightCount;
         outconfig.TextureQuality = QualitySettings.masterTextureLimit;
         outconfig.AnisotropicTextures = QualitySettings.anisotropicFiltering.ToString();
-        outconfig.AntiAliasing = QualitySettings.antiAliasing;
         outconfig.SoftParticles = QualitySettings.softParticles;
         outconfig.RealtimeReflectionProbes = QualitySettings.realtimeReflectionProbes;
+        outconfig.ReflectionProbesResolution = 64;
+        outconfig.ReflectionProbesRefresh = 0;
         outconfig.BillboardsFaceCameraPosition = QualitySettings.billboardsFaceCameraPosition;
         outconfig.ResolutionScalingFixedDPI = QualitySettings.resolutionScalingFixedDPIFactor;
         outconfig.TextureStreaming = QualitySettings.streamingMipmapsActive;
@@ -43,10 +56,21 @@ public class GameConfigHandlerScript : MonoBehaviour
     //Turn GameConfig class into current game config
     private void LoadConfig(GameConfig inconfig)
     {
+        //Set post-processing   
+        SetPostProcessing(
+            inconfig.usePostProcessing,
+            inconfig.useColorGrading,
+            inconfig.useChromaticAberration,
+            inconfig.useBloom,
+            inconfig.useVignette,
+            inconfig.useAutoExposure,
+            inconfig.useMotionBlur,
+            inconfig.useAmbientOcclusion,
+            inconfig.useAntiAliasing
+        );
         QualitySettings.pixelLightCount = inconfig.PixelLightCount;
         QualitySettings.masterTextureLimit = inconfig.TextureQuality;
         QualitySettings.anisotropicFiltering = (AnisotropicFiltering)System.Enum.Parse(typeof(AnisotropicFiltering), inconfig.AnisotropicTextures);
-        QualitySettings.antiAliasing = inconfig.AntiAliasing;
         QualitySettings.softParticles = inconfig.SoftParticles;
         QualitySettings.realtimeReflectionProbes = inconfig.RealtimeReflectionProbes;
         SetReflectionProbeResolution(inconfig.ReflectionProbesResolution);
@@ -86,5 +110,32 @@ public class GameConfigHandlerScript : MonoBehaviour
             if (type == 1)
                 probes[i].refreshMode = UnityEngine.Rendering.ReflectionProbeRefreshMode.EveryFrame;//Lower quality and static reflection only
         }
+    }
+    //Changes if we use post-processing or not
+    private void SetPostProcessing(bool usepostprocessing, bool colorgrading, bool chromaticaberration, bool bloom, bool vignette, bool autoexposure, bool motionblur, bool ambientocclusion, int antialiasing) 
+    {
+        PostProcessLayer layer = GameObject.FindObjectOfType<PostProcessLayer>();//Get the camera post-processing
+        if (antialiasing != 1 || antialiasing != 2 || antialiasing != 3) layer.antialiasingMode = PostProcessLayer.Antialiasing.None;
+        if (antialiasing == 1) layer.antialiasingMode = PostProcessLayer.Antialiasing.FastApproximateAntialiasing;
+        if (antialiasing == 2) layer.antialiasingMode = PostProcessLayer.Antialiasing.SubpixelMorphologicalAntialiasing;
+        if (antialiasing == 3) layer.antialiasingMode = PostProcessLayer.Antialiasing.TemporalAntialiasing;
+        PostProcessVolume[] volumes = GameObject.FindObjectsOfType<PostProcessVolume>();//Get all post-processing volumes so we can change every post-processing profile
+        for (int i = 0; i < volumes.Length; i++)
+        {
+            PostProcessProfile postprocess = volumes[i].sharedProfile;
+            //Set post-process effects
+            for(int s = 0; s < postprocess.settings.Count; s++) 
+            {
+                if (s == 0) postprocess.settings[s].active = colorgrading;
+                if (s == 1) postprocess.settings[s].active = chromaticaberration;
+                if (s == 2) postprocess.settings[s].active = bloom;
+                if (s == 3) postprocess.settings[s].active = vignette;
+                if (s == 4) postprocess.settings[s].active = autoexposure;
+                if (s == 5) postprocess.settings[s].active = motionblur;
+                if (s == 6) postprocess.settings[s].active = ambientocclusion;
+            }
+        }
+
+        layer.enabled = usepostprocessing;//If we are 
     }
 }
